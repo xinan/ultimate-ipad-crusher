c85fdf8c8db8770dcec634649b05f5fd42aef085
Liu Xinan <xinan@me.com>
2015-11-06 21:33:17 +0800

    Fix combinatorics code

--- code/xinan/combinatorics.py
+++ code/xinan/combinatorics.py
@@ -5,10 +5,10 @@ from scipy.misc import comb
 
 def trials(N, M):
   T = int(log(N, 2))
+  if M >= T:
+    return T
   while (True):
-    total = sum(comb(T, k) for k in xrange(0, M + 1))
-    if (total >= N):
+    total = sum(round(comb(T, k)) for k in xrange(0, M + 1))
+    if (total > N):
       return T
     T += 1
-    
-print trials(100000, 2)
